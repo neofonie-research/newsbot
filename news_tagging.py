d4913c340c1b9453fbc5c26a4931ad16c6d06605
Bertram Sändig <bertram.saendig@neofonie.de>
2019-09-17 17:16:25 +0200

    some cleaning up, error handling example elast_requests.http

--- news_tagging.py
+++ news_tagging.py
@@ -4,36 +4,50 @@ from elasticsearch import Elasticsearch
 NEWS_API_KEY = "YOUR_NEWS_KEY"
 TXT_WERK_KEY = "YOUR_TXTWERK_KEY"
 es_index = 'news_articles'
+## regex to remove remaining character counts ([+950 chars]) at the end of news article responses
 regex_chars = re.compile(r'\[\+[0-9]* chars\]')
 
 def txt_werk_request(text, services = 'categories,entities,tags'):
    headers = {'X-API-Key': TXT_WERK_KEY}
    url = "https://api.neofonie.de/rest/txt/analyzer"
    data = {'text':text.encode('utf-8') , 'services': services}
-   res = requests.post(url, data=data, headers=headers)
-   return res.json()
+   resp = requests.post(url, data=data, headers=headers).json()
+   if not 'entities' in resp and not 'tags' in resp:
+      print(resp)
+   return resp
+
+def get_article_text(article):
+   text = '. '.join([article[field] for field in ['title','description','content'] if article[field]])
+   ## remove remaining character counts in responses
+   return regex_chars.sub("", text)
+
+def get_tags(metadata):
+   return list(set(
+      [x['surface'] for x in metadata['entities'] if x['surface'] is not None] \
+      + [x['label'] for x in metadata['entities'] if x['label'] is not None] \
+      + [x['term'] for x in metadata['tags'] if x['term'] is not None]
+      ))
 
 def index_news(sources):
-   headlines = requests.get("https://newsapi.org/v2/top-headlines?sources={}&apiKey={}&pageSize=100".format(sources,NEWS_API_KEY)).json()
-
    es = Elasticsearch(['localhost'])
-
+   url = "https://newsapi.org/v2/top-headlines?sources={}&apiKey={}&pageSize=100"
+   headlines = requests.get(url.format(sources,NEWS_API_KEY)).json()
+   if not 'articles' in headlines:
+      print(headlines)
    for article in headlines['articles']:
-      text = '. '.join([article[field] for field in ['title','description','content'] if article[field]])
-      text = regex_chars.sub("", text)
+      text = get_article_text(article)
       metadata = txt_werk_request(text)
       doc = {
          'title': article['title'],
          'description': article['description'],
          'content': article['content'],
          'url': article['url'],
-         'tags': list(set([x['surface'] for x in metadata['entities']] + [x['label'] for x in metadata['entities']] + [x['term'] for x in metadata['tags']])),
+         'tags': get_tags(metadata),
          'category': metadata['categories'][0]['label'].capitalize()
          }
-      print(doc['title'])
-      print(doc['url'])
-      print(doc['tags'])
-      res = es.index(index=es_index, doc_type='_doc', body=doc)
+      print(doc['title'], doc['tags'], sep='\n')
+      ## url as _id to avoid duplicates
+      resp = es.index(es_index, doc_type='_doc', body=doc, id=doc['url'])
 
 if __name__ == "__main__":
    sources = "spiegel-online,bild,focus,die-zeit,wired-de,der-tagesspiegel,t3n,handelsblatt"
